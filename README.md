0e5f0b0a0ce2778676afaa03d4f3ef0a73bb3bb9
Brazen <764146802@qq.com>
2019-05-21 23:26:22 +0800

    加入parcel,将button组件改为单文件组件

--- README.md
+++ README.md
@@ -1,3 +1,9 @@
 # 轱辘 - 一个 Vue UI 组件
 
 作者 : Brazen
+
+## 安装
+使用本框架，请在CSS中开启 border-box
+```
+*{box-sizing:border-box}
+```
---
+++ src/button.vue
@@ -0,0 +1,40 @@
+<template>
+  <button class="g-button">按钮</button>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style lang="scss">
+  .g-button{
+    font-size: var(--font-size);
+    height: var(--button-height);
+    padding:0 0.9em 1px;
+    border-radius: var(--border-radius);
+    border:1px solid var(--border-color);
+    background: var(--button-bg);
+    display: inline-block;
+    display: inline-flex;
+    justify-content: center;
+    align-items: center;
+    vertical-align: top;
+    &:hover{
+      border-color:var(--border-color-hover)
+    }
+    &:active{
+      background-color:var(--button-active-bg)
+    }
+    &:focus{ outline: none;}
+    &.icon-left{
+      > .icon{order: 1 ;margin-right: 0.3em;}
+      > .content{order: 2;}
+    }
+    &.icon-right{
+      > .icon{order: 2;margin-right: 0;margin-left: 0.3em;}
+      > .content{order: 1;}
+    }
+  }
+</style>
