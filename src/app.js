45e166c16a3c02b19a69233a7bc85b1554894d26
Brazen <764146802@qq.com>
2019-05-25 17:57:31 +0800

    添加 toast 和 plugin

--- src/app.js
+++ src/app.js
@@ -10,6 +10,8 @@ import Content from './content.vue'
 import Footer from './footer.vue'
 import Layout from './layout.vue'
 import Sider from './sider.vue'
+import Toast from './toast.vue'
+import Plugin from './plugin.js'
 
 Vue.component('g-button',Button)
 Vue.component('g-icon',Icon)
@@ -22,6 +24,9 @@ Vue.component('g-content',Content)
 Vue.component('g-footer',Footer)
 Vue.component('g-layout',Layout)
 Vue.component('g-sider',Sider)
+Vue.component('g-toast',Toast)
+Vue.use(Plugin)
+
 
 new Vue({
   el:'#app',
@@ -31,93 +36,12 @@ new Vue({
     loading3:false,
     message:'hi'
   },
+  created(){
+
+  },
   methods: {
-    changeInput(e){
-      console.log(1);
-      console.log(e.target.value);
-    }
+    showToast(message){
+      this.$toast('我是message')
+    },
   },
 })
-
-// //单元测试
-// import chai from 'chai'
-// import spies from 'chai-spies'
-// chai.use(spies)
-// let expect = chai.expect
-// {
-//   // 测试按钮含有icon
-//   const Constructor = Vue.extend(Button)
-//   const vm = new Constructor({
-//     propsData:{
-//       icon:'settings'
-//     }
-//   })
-//   vm.$mount()
-//   let useElement = vm.$el.querySelector('use')
-//   let href = useElement.getAttribute('xlink:href')
-//   expect(href).to.eq('#i-settings')
-//   vm.$el.remove()
-//   vm.$destroy()
-// }
-// {
-//   const Constructor = Vue.extend(Button)
-//   const vm = new Constructor({
-//     propsData:{
-//       icon:'settings',
-//       loading:true
-//     }
-//   })
-//   vm.$mount()
-//   let useElement = vm.$el.querySelector('use')
-//   let href = useElement.getAttribute('xlink:href')
-//   expect(href).to.eq('#i-loading')
-//   vm.$el.remove()
-//   vm.$destroy()
-// }
-// {
-//   const div = document.createElement('div')
-//   document.body.appendChild(div)
-//   const Constructor = Vue.extend(Button)
-//   const vm = new Constructor({
-//     propsData:{
-//       icon:'settings',
-//     }
-//   })
-//   vm.$mount(div)
-//   let svg = vm.$el.querySelector('svg')
-//   let {order} = window.getComputedStyle(svg)
-//   expect(order).to.eq('1')
-//   vm.$el.remove()
-//   vm.$destroy()
-// }
-// {
-//   const div = document.createElement('div')
-//   document.body.appendChild(div)
-//   const Constructor = Vue.extend(Button)
-//   const vm = new Constructor({
-//     propsData:{
-//       icon:'settings',
-//       iconPosition:'right'
-//     }
-//   })
-//   vm.$mount(div)
-//   let svg = vm.$el.querySelector('svg')
-//   let {order} = window.getComputedStyle(svg)
-//   expect(order).to.eq('2')
-//   vm.$el.remove()
-//   vm.$destroy()
-// }
-// {
-//   const Constructor = Vue.extend(Button)
-//   const vm = new Constructor({
-//     propsData:{
-//       icon:'settings',
-//     }
-//   })
-//   vm.$mount()
-//   let spy = chai.spy(function(){})
-//   vm.$on('click',spy)
-//   let button = vm.$el
-//   button.click()
-//   expect(spy).to.have.been.called()
-// }
---
+++ src/toast.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="toast">
+    <slot></slot>
+  <div>
+</template>
+
+<script>
+export default {
+  name:'GuluToast',
+  data(){
+    return {
+
+    }
+  }
+}
+</script>
+
+<style lang='scss' scoped>
+  $toast-height: 40px;
+  $font-size: 14px;
+  $toast-bg:rgba(0,0,0,0.75);
+  .toast{
+    height: $toast-height; font-size: $font-size; position: fixed;
+    top: 0; left:50%; transform: translateX(-50%); line-height:1.8;
+    display: flex; align-items: center;
+    background: $toast-bg;
+    border-radius: 4px;
+    box-shadow: 0px 0px 3px 0px rgba(0,0,0,0.5);
+    padding: 0 16px;
+    color: white;
+  }
+</style>
