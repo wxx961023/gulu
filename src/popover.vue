232f965c6c40dc3ccd5652be1daeeedf2d67483e
Brazen <764146802@qq.com>
2019-05-28 15:45:00 +0800

    完成popover基本功能

--- src/popover.vue
+++ src/popover.vue
@@ -1,6 +1,6 @@
 <template>
-  <div class="popover" @click.stop="xxx">
-    <div ref="contentWrapper" class="content-wrapper" v-if="visible" @click.stop>
+  <div class="popover" @click="onClick" ref="popover">
+    <div ref="contentWrapper" class="content-wrapper" v-if="visible">
       <slot name="content"></slot>
     </div>
     <span ref="triggerWrapper">
@@ -13,25 +13,45 @@
 export default {
   name:'GuluPopover',
   data(){
-    return {
-      visible:false,
-    }
+    return {visible:false}
   },
   methods:{
-    xxx(){
-      this.visible = !this.visible
-      if(this.visible === true){
-        this.$nextTick(()=>{
-          document.body.appendChild(this.$refs.contentWrapper)
-          let {width,top,left,right} = this.$refs.triggerWrapper.getBoundingClientRect()
-          this.$refs.contentWrapper.style.left = left + window.scrollY +'px'
-          this.$refs.contentWrapper.style.top = top+window.scrollX+'px'
-          let eventHandler = ()=>{
-            this.visible = false
-            document.removeEventListener('click',eventHandler)
-          }
-          document.addEventListener('click',eventHandler)
-        })
+    positionContent(){
+      document.body.appendChild(this.$refs.contentWrapper)
+      let {width,height,top,left} = this.$refs.triggerWrapper.getBoundingClientRect()
+      this.$refs.contentWrapper.style.left = left + window.scrollX + 'px'
+      this.$refs.contentWrapper.style.top = top + window.scrollY + 'px'
+    },
+    onClickDocument(e){
+      if(this.$refs.popover && 
+        (this.$refs.popover === e.target || this.$refs.popover.contains(e.target))
+      ){return}
+      if(this.$refs.contentWrapper &&
+        (this.$refs.contentWrapper === e.target || this.$refs.contentWrapper.contains(e.target))
+      ) { return }
+      console.log('你不应该看到这');
+      this.close()
+    },
+    open(){
+      this.visible = true
+      this.$nextTick(()=>{
+        this.positionContent()
+        document.addEventListener('click',this.onClickDocument)
+      })
+    },
+    close(){
+      this.visible = false
+      document.removeEventListener('click',this.onClickDocument)
+    },
+
+    onClick(event){
+      console.log(1);
+      if(this.$refs.triggerWrapper.contains(event.target)){
+        if(this.visible === true){
+          this.close()
+        }else{
+          this.open()
+        }
       }
     }
   }
