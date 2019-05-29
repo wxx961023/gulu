7fcfb9a09a479410fa27fedfdd68cc6436101be1
Brazen <764146802@qq.com>
2019-05-30 00:21:43 +0800

    官网基本雏形

--- docs/.vuepress/components/button-demos.vue
+++ docs/.vuepress/components/button-demos.vue
@@ -5,11 +5,7 @@
     <g-button :loading="true">默认按钮</g-button>
     <g-button disabled>默认按钮</g-button>
 
-    <pre>
-      <code>
-        {{content}}
-      </code>
-    </pre>
+    <pre><code>{{content}}</code></pre>
   </div>
 </template>
 
@@ -26,7 +22,7 @@
           <g-button icon="settings">默认按钮</g-button>
           <g-button :loading="true">默认按钮</g-button>
           <g-button disabled>默认按钮</g-button>
-        `
+        `.replace(/\t+| +/g, '').trim()
       }
     }
     
--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -13,7 +13,15 @@ module.exports = {
       }, 
       {
         title:'组件',
-        children:['/components/button']
+        children:[
+          '/components/button',
+          '/components/tabs',
+          '/components/input',
+          '/components/grid',
+          '/components/layout',
+          '/components/toast', 
+          '/components/tabs',          
+        ]
       }
     ]
   }
